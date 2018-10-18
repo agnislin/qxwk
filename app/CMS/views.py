4152444ab8e46070139e82f786246c21a3041dc1
cfj-github <1950546041@qq.com>
2018-10-18 16:42:44 +0800

    change

--- app/CMS/views.py
+++ app/CMS/views.py
@@ -21,3 +21,40 @@ def show_username(username):
         return user + course
 
 
+@cms.route('/reurl',methods=['GET','POST'])
+def reurl():
+    print("resul")
+    if request.method == 'POST':
+        path = request.form['page']
+        print(path)
+        return render_template("cms/%s.html" % path)
+
+
+
+
+
+@cms.route('/comment',methods=['GET','POST'])
+def comment(comment_id):
+    if request.method == 'GET':
+        return render_template('/comlist')
+    elif request.method == 'POST':
+        pass
+
+
+
+@cms.route('/course',methods=['GET','POST'])
+def course():
+    if request.method == 'GET':
+        return render_template('/coulist')
+    elif request.method == 'POST':
+        course_id = request.form['course_id']
+        course_name = request.form['course_name']
+        do_course(course_id)
+
+
+
+# @cms.log('/log',method=['GET','POST'])
+# def log():
+#     return render_template('log.html')
+
+
