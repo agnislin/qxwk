97b96f1229f3f5e9c4c6d28361e079a0f2163f67
agnis <1174793397@qq.com>
2018-10-29 18:57:45 +0800

    agnis

--- main/server/forum/comment.py
+++ main/server/forum/comment.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from flask_sqlalchemy import SQLAlchemy
+
 import models
 from entry import *
-import main.tools.tools as tl
-# from .auth import seesion_value
+from main.server.user_center.account import *
 
 
-def get_comment_by_video_id(video_id):
+def get_comment_by_video_id(video_id, limit=1):
+    data = list()
+    coms = models.find(Comment, Comment.video_id == video_id, limit)
 
-    data = []
-    coms = models.find(e.Comment,e.Comment.video_id == vid)
     for comment in coms:
-        account = models.find(e.UserInfo,e.UserInfo.account_id == comment.account_id)[0]
+        account = models.find(UserInfo, UserInfo.account_id == comment.account_id)[0]
         data.append((account, comment))
+    print("get_comment_by_video_id", data)
+    return data
 
-    # Return list of posts.
-    return render_template('server/forum.html', forum_list =data)
+
+def save_comment(video_id, content):
+    data = dict()
+    data["account_id"] = get_account_id()
+    data["video_id"] = video_id
+    data["comment"] = content
+    c = Comment(**data)
+    models.save(c)
+    return c
